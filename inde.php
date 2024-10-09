53053ee59159e3b22e65942fd49b5199b15f2dc2
rhaldane92 <148106070+rhaldane92@users.noreply.github.com>
2024-10-09 02:23:55 +0100

    Add files via upload

---
+++ inde.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>rowhald.com</title>
+	<style type="text/css">
+		*{
+			text-decoration: none;
+		}
+		.navbar{
+			background: rgb(20, 160, 220); font-family: calibri; padding-right: 15px;padding-left: 15px;
+		}
+		.navdiv{
+			display: flex; align-items: center; justify-content: space-between;
+		}
+		.logo a{
+			font-size: 35px; font-weight: 600; color: white;
+		}
+		li{
+			list-style: none; display: inline-block;
+		}
+		li a{
+			color: white; font-size: 18px; font-weight: bold; margin-right: 25px;
+		}
+	</style>
+</head>
+<body style="background-color:powderblue;">
+	<nav class="navbar">
+		<div class="navdiv">
+			<div class="logo"><a href="index.html">rowhald.com</a> </div>
+			<ul>
+				<li><a class="active" href="videos.html">Videos</a></li>
+				<li><a class="active" href="music.html">Music</a></li>
+				<li><a class="active" href="index.php">Login</a></li>
+			</ul>
+		</div>
+	</nav>
+</body>
+</html>
